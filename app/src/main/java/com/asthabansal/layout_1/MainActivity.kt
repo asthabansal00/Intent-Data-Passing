fc2a1d75f76e9c16dfd0f28d799673f79f6f30fe
asthabansal00 <138471247+asthabansal00@users.noreply.github.com>
2023-07-04 17:20:49 +0530

    layout changes

--- app/src/main/java/com/asthabansal/layout_1/MainActivity.kt
+++ app/src/main/java/com/asthabansal/layout_1/MainActivity.kt
@@ -3,23 +3,25 @@ package com.asthabansal.layout_1
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
-    var name=""
+    var Full_Name:TextView?=null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Full_Name=findViewById(R.id.etname)
 
-        intent?.let{
-            name=it.getStringExtra("name")?:""
-            System.out.println("name $name")
-        }
+        /*intent?.let{
+            Full_Name=it.getStringExtra("name")?:""
+            System.out.println("Full_Name $Full_Name")
+        }*/
 
-        var intent=Intent(this,MainActivity2::class.java)
-        intent.putExtra("name","ASTHA")
-        startActivity(intent)
+        /*var intent=Intent(this,MainActivity2::class.java)
+        intent.putExtra("Full_Name","ASTHA")
+        startActivity(intent)*/
     }
 
     override fun onStart() {
